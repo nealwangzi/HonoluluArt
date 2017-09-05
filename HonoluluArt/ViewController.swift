58a85d330e9d20000ca5ec47e4168c8b81b2badb
neal <caoyaha@cloudoc.cn>
2017-09-05 17:31:51 +0800

    Add 获取json数据，展示对应大头针数据

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var artworks = [Artwork]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,13 +24,51 @@ class ViewController: UIViewController {
         
         centerMapOnLocation(location: initialLocation)
         
-        let artwork = Artwork(title: "kind david kalakaua",
-                              locationName: "waikiki gateway park",
-                              discipline: "sculpture", coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
-        mapView.addAnnotation(artwork)
+//        let artwork = Artwork(title: "kind david kalakaua",
+//                              locationName: "waikiki gateway park",
+//                              discipline: "sculpture", coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+//        mapView.addAnnotation(artwork)
         mapView.delegate = self
+        
+        loadInitialData()
+        mapView.addAnnotations(artworks)
     }
     
+    
+
+    func loadInitialData() {
+        //1
+        let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json")
+        var data: Data?
+        do {
+            data = try Data(contentsOf: URL(fileURLWithPath: fileName!), options: Data.ReadingOptions.init(rawValue: 0))
+        } catch _ {
+            data = nil
+        }
+        
+        //2
+        var jsonObject:Any? = nil
+        if let data = data {
+            do {
+                jsonObject = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.init(rawValue: 0))
+            } catch _ {
+                jsonObject = nil
+            }
+        }
+        
+        //3
+        if let jsonObject = jsonObject as? [String: Any],
+            let jsonData = JSONValue.fromObject(jsonObject)?["data"]?.array
+        {
+            for artworkJSON in jsonData {
+                if let artworkJSON = artworkJSON.array,
+                    let artwork = Artwork.fromJSON(artworkJSON)
+                {
+                    artworks.append(artwork)
+                }
+            }
+        }
+    }
     let regionRadius:CLLocationDistance = 1000
 
     func centerMapOnLocation(location: CLLocation) {
