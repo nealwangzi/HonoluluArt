15f64fabcc5c170bdb1d2db2fdf26dd02690c3bf
neal <caoyaha@cloudoc.cn>
2017-09-05 16:56:02 +0800

    Add 初始化项目 1.使用mapkit 2.暂时锚点， 3.定位锚点

--- HonoluluArt/ViewController.swift
+++ HonoluluArt/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 设置可见区域
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        
+        centerMapOnLocation(location: initialLocation)
+        
+        let artwork = Artwork(title: "kind david kalakaua",
+                              locationName: "waikiki gateway park",
+                              discipline: "sculpture", coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        mapView.addAnnotation(artwork)
+        mapView.delegate = self
     }
+    
+    let regionRadius:CLLocationDistance = 1000
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
-
 
 }
 
